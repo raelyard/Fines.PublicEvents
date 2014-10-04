8b87678856aa3b4907dc377e7d4bd36e0f6e2c76
Dave Rael <dave@raelyard.com>
2014-10-04 11:44:04 -0600

    Added event for communication that a fine has been calculated

---
+++ Fines.PublicEvents/FineCalculatedEvent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MediaLoanLibrary.Fines.PublicEvents
+{
+    public interface FineCalculatedEvent
+    {
+        Guid LoanId { get; set; }
+        decimal FineAmount { get; set; }
+    }
+}
